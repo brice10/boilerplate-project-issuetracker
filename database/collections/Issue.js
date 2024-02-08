502dcc90aef480511f06b7f36c122db67cd76961
Arléon <arleonzemtsop@gmail.com>
2024-02-08 18:53:11 +0100

    Define required fields, optionnal fields and default field value in Issue Schema. Define save hook to set up createdOn and UpdatedOn on data save

--- database/collections/Issue.js
+++ database/collections/Issue.js
@@ -3,42 +3,58 @@ const mongoose = require('mongoose');
 // MongoDB collection for Issues
 class Issue {
     static _model;
-    
+
     constructor(collectionName) {
         this._collectionName = collectionName || 'Issue';
         this._model = mongoose.model(this._collectionName, this._schema);
+
+        this._schema.pre('save', function (next) {
+            if (!this.created_on) {
+                this.created_on = new Date();
+            }
+            this.updated_on = new Date();
+            next()
+        })
     }
 
     _schema = new mongoose.Schema({
         project_name: {
-            type: String
+            type: String,
+            require: true,
+            select: false
         },
         _id: {
             type: String
         },
         issue_title: {
-            type: String
+            type: String,
+            require: true
         },
         issue_text: {
-            type: String
+            type: String,
+            require: true,
+        },
+        created_by: {
+            type: String,
+            require: true,
+        },
+        assigned_to: {
+            type: String,
+            default: ''
+        },
+        status_text: {
+            type: String,
+            default: ''
+        },
+        open: {
+            type: Boolean,
+            default: true
         },
         created_on: {
             type: Date
         },
         updated_on: {
             type: Date
-        },
-        created_by: {
-            type: String
-        },
-        assigned_to: {
-            type: String
-        },
-        open: {
-            type: Boolean
-        },
-        status_text: {
-            type: String
         }
     })
 }
